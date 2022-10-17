d4f72fa9d90d8b7f3cef9ceb9cf119577eb4e55e
Kémeri Csanád <kemeri.csanad@gmail.com>
2022-10-17 12:56:06 +0200

    Installer progress

--- ssl_pinning_installer.bat
+++ ssl_pinning_installer.bat
@@ -3,3 +3,11 @@ pip install frida-tools
 frida --version
 pip install objection
 
+powershell -Command "Invoke-WebRequest https://github.com/Splash250/nox-ssl-pinning-bypass-utils/blob/remote_files/frida-server?raw=true -OutFile frida-server"
+
+cls
+.\nox_adb.exe push .\frida-server /data/local/tmp
+
+.\nox_adb.exe shell "chmod 777 /data/local/tmp/frida-server"
+
+pause
